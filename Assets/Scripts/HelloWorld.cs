4bf51425475fc101b0798948c9b1498bf5d79505
Boris <45284853+Brovin@users.noreply.github.com>
2019-01-27 21:56:17 +0300

    update

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public bool on = false;
+    public int level = 1;
+    public string text = "Hello_World";
+
+    void Awake() {
+        Debug.Log("Level is up to load");
+    }
+    void Start()
+    {
+        Debug.Log("Level is load");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(on == true && level < 10) {
+            level += 1;
+            Debug.Log(text + " your level is = " + level);
+        }
+    }
+}
---
+++ Assets/Scripts/RotationObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObject : MonoBehaviour
+{
+    public Vector3 Dirrection;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Dirrection * Time.deltaTime * speed);
+    }
+}
