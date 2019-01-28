1962e1cb0e30097f161b1bc682ea4445349c5158
Boris <45284853+Brovin@users.noreply.github.com>
2019-01-28 23:41:44 +0300

    update

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float speed;
+    public Transform a;
+    public UnityEventQueueSystem v;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(true){
+                transform.position = Vector3.MoveTowards(transform.position, a.position, Time.deltaTime * speed);
+            }
+        }
+    }
+}
