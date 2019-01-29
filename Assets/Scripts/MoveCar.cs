9daf1423a385cfe42a97232ef39646e98362bd93
Boris <45284853+Brovin@users.noreply.github.com>
2019-01-29 23:00:03 +0300

    update

--- Assets/Scripts/MoveCar.cs
+++ Assets/Scripts/MoveCar.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 public class MoveCar : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed = 7f;
+    public float speed;
+    public float now;
+    public float _1range;
+    public float _2range;
+    public Vector3 rotate;
+    bool c = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,11 +20,30 @@ public class MoveCar : MonoBehaviour
     
     void Update()
     {
-        straight();
+        if(c == true)
+        {
+            straight();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        c = true;
     }
 
     void straight()
     {
         rb.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+        float carRotate = Mathf.Floor(transform.eulerAngles.y);
+         if (transform.position.z > now)
+            {
+                if (carRotate >= _1range && carRotate <= _2range)
+                {
+                    transform.localRotation = Quaternion.Euler(rotate);
+                    return;
+                }
+                Quaternion a = Quaternion.Euler(rotate * Time.fixedDeltaTime);
+                rb.MoveRotation(transform.rotation * a);
+        }
     }
 }
